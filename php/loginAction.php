a1e614b0d71e120bb206cc15e2fc58a154c63e40
JoheeYeom0103 <152527665+JoheeYeom0103@users.noreply.github.com>
2024-04-12 20:23:38 -0700

    Update loginAction.php

--- php/loginAction.php
+++ php/loginAction.php
@@ -1,9 +1,11 @@
 <?php
 
 session_start();
-include("dbConnect.php");
+
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    include("dbConnect.php");
     
     $userID = $_POST['username'];
     $password = $_POST['password'];
